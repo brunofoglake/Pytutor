96483ed755e7819730193f80f5969f539d1ad187
brunofoglake <brunoeduardonatal@gmail.com>
2026-07-09 19:11:25 -0300

    Mudanças para o github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Pytutor/', // troque pelo nome exato do seu repositório no GitHub
 })
